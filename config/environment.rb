6dbe148612c945b488550cbd83162386b18710f0
Eric Zeiberg <masterejzz4820@gmail.com>
2014-08-15 15:42:41 -0400

    now plz

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-ENV['SSL_CERT_FILE'] = Rails.root.join('/cacert.pem').to_s
+ENV['SSL_CERT_FILE'] = "/root/Blog/Blog/cacert.pem"
 # Initialize the rails application
 Blog::Application.initialize!
